4ab3241da4de9253ad9ed0bc9b5da903399e19e4
Ola Ajala <ola.ajala1@outlook.com>
2018-05-28 09:16:08 -0600

    updating packages in CompleteTwitterWorkflowTest.java

--- src/test/java/twitter/CompleteTwitterWorkflowTest.java
+++ src/test/java/twitter/CompleteTwitterWorkflowTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.authentication.AuthenticationScheme;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
