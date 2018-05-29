419b698460cc008eca8291f88e2e03ef2f800266
Ola Ajala <ola.ajala1@outlook.com>
2018-05-29 17:57:30 -0600

    adding restutilities class

--- src/test/java/com/frameworkBuilder/common/RestUtilities.java
+++ src/test/java/com/frameworkBuilder/common/RestUtilities.java
@@ -10,8 +10,12 @@ import io.restassured.authentication.AuthenticationScheme;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import static io.restassured.RestAssured.*;
 
 public class RestUtilities {
 
@@ -58,6 +62,47 @@ public class RestUtilities {
 
 	public static RequestSpecification createPathParams(RequestSpecification rspec, Map<String, String> pathMaps){
 		return rspec.pathParams(pathMaps);
+	}
+
+	public static Response getResponse(){
+		return given().get(ENDPOINT);
+	}
+	
+	public static Response getResponse(RequestSpecification recSpec, String type) {
+		REQUEST_SPEC.spec(recSpec);
+		Response response = null;
+		if (type.equalsIgnoreCase("get")) {
+			response = given().spec(REQUEST_SPEC).when().get(ENDPOINT);
+		} else if (type.equalsIgnoreCase("post")) {
+			response = given().spec(REQUEST_SPEC).when().post(ENDPOINT);
+		} else if (type.equalsIgnoreCase("put")) {
+			response = given().spec(REQUEST_SPEC).when().put(ENDPOINT);
+		} else if (type.equalsIgnoreCase("delete")) {
+			response = given().spec(REQUEST_SPEC).when().delete(ENDPOINT);
+		} else {
+			System.out.println("This type is not supported");
+		}
+		response.then().log().all();
+		response.then().spec(RESPONSE_SPEC);
+		return response;
+	}
+	
+	public static JsonPath getJsonPath(Response res){
+		String jsonPath = res.asString();
+		return new JsonPath(jsonPath);
+	}
+	
+	public static XmlPath getXmlPath(Response res){
+		String xmlPath = res.asString();
+		return new XmlPath(xmlPath);
+	}
+	
+	public static void resetBasePath(){
+		RestAssured.basePath = null;
+	}
+	
+	public static void setContentType(ContentType type){
+		given().contentType(type);
 	}
 
 }
